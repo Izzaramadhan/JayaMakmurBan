eed632e87b889dbce11483f45abaef1a6f2be862
Ubayahmad11 <ubayahmad@students.amikom.ac.id>
2026-05-23 22:52:42 +0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Catalog from "./components/Catalog";
 import Services from "./components/Services";
+import BookingSteps from "./components/BookingSteps";
 import BookingForm from "./components/BookingForm";
 import Footer from "./components/Footer";
 
@@ -15,6 +16,7 @@ export default function App() {
         <Features />
         <Catalog />
         <Services />
+        <BookingSteps/>
         <BookingForm />
       </main>
       <Footer />
